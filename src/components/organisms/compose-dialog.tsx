77dd2315029836bf1a2425bcc9cc71746d645d4d
LinoRino <omurakenshin@gmai.com>
2023-09-24 17:13:29 +0900

    add dialog close component

--- src/components/organisms/compose-dialog.tsx
+++ src/components/organisms/compose-dialog.tsx
@@ -1,44 +1,70 @@
 "use client";
 
-import { Dialog, DialogContent, DialogFooter, DialogTitle } from "../ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from "../ui/dialog";
 import { DialogHeader } from "../ui/dialog";
 import { Textarea } from "../ui/textarea";
 import { Column, Row } from "../ui/layout";
 import { Button } from "../ui/button";
 import { ImageIcon, LaughIcon } from "lucide-react";
 import type { FormHTMLAttributes } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ComposeDialog({
   action,
 }: {
   action: FormHTMLAttributes<HTMLFormElement>["action"];
 }) {
+  const router = useRouter();
   return (
-    <form action={action} onSubmit={console.log}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Componse</DialogTitle>
-        </DialogHeader>
-        <Column spacing={2}>
-          <Textarea
-            id="content"
-            name="content"
-            placeholder="What's happening?"
-            className="w-full resize-none"
-          />
-        </Column>
-        <DialogFooter>
-          <Row spacing={1} className="w-full">
-            <Button tip="Upload media" variant="ghost" size="icon">
-              <ImageIcon strokeWidth={1.75} />
-            </Button>
-            <Button tip="Pick emoji" variant="ghost" size="icon">
-              <LaughIcon strokeWidth={1.75} />
-            </Button>
-          </Row>
-          <Button type="submit">Submit</Button>
-        </DialogFooter>
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          router.back();
+        }
+      }}
+      open
+    >
+      <DialogContent asChild>
+        <form
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && event.metaKey) {
+              console.log("submit");
+            }
+          }}
+          action={action}
+          onSubmit={console.log}
+        >
+          <DialogHeader>
+            <DialogTitle>Componse</DialogTitle>
+          </DialogHeader>
+          <Column spacing={2}>
+            <Textarea
+              id="content"
+              name="content"
+              placeholder="What's happening?"
+              className="w-full resize-none"
+            />
+          </Column>
+          <DialogFooter>
+            <Row spacing={1} className="w-full">
+              <Button tip="Upload media" variant="ghost" size="icon">
+                <ImageIcon strokeWidth={1.75} />
+              </Button>
+              <Button tip="Pick emoji" variant="ghost" size="icon">
+                <LaughIcon strokeWidth={1.75} />
+              </Button>
+            </Row>
+            <Button type="submit">Submit</Button>
+          </DialogFooter>
+          <DialogClose />
+        </form>
       </DialogContent>
-    </form>
+    </Dialog>
   );
 }
