fd8ae42cf10ea748dd104dbe2fdc47ee182067e8
LinoRino <omurakenshin@gmal.com>
2023-10-27 13:58:03 +0900

    component-ize the item of portal-menu

--- src/components/organisms/portal-menu.tsx
+++ src/components/organisms/portal-menu.tsx
@@ -4,37 +4,43 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { Column } from "../ui/layout";
 import DigitalClock from "./digital-clock";
-import { Feather, Home, Settings2, User } from "lucide-react";
+import {
+  Feather,
+  Home,
+  type LucideIcon,
+  Settings2,
+  User,
+  CheckCheck,
+} from "lucide-react";
+
+interface ItemProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
+
+function Item({ href, icon: Icon, label }: ItemProps) {
+  return (
+    <li>
+      <Button asChild variant="link">
+        <Link href={href} className="flex gap-4">
+          <Icon />
+          {label}
+        </Link>
+      </Button>
+    </li>
+  );
+}
 
 export default function PortalMenu() {
   return (
     <Column element="aside" gap={4} className="p-6">
       <DigitalClock />
       <Column element="ul" className="gap-2 mb-8">
-        <li>
-          <Button asChild variant="link">
-            <Link href="/" className={"flex gap-4"}>
-              <Home />
-              Entrance
-            </Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild variant="link">
-            <Link href="/" className="flex gap-4">
-              <User />
-              Profile
-            </Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild variant="link">
-            <Link href="/setting" className="flex gap-4">
-              <Settings2 />
-              Setting
-            </Link>
-          </Button>
-        </li>
+        <Item href="/" icon={Home} label="Home" />
+        <Item href="/" icon={User} label="Profile" />
+        <Item href="/setting" icon={Settings2} label="Setting" />
+        <Item href="/todo" icon={CheckCheck} label="Todo" />
       </Column>
       <Button asChild size="lg">
         <Link href="compose">
